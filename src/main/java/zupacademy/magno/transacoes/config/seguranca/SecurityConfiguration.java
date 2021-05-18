07ae38adb80c25ba531ea9f4f0b1767ae07b2eaf
magnoazneto <magnoazneto@gmail.com>
2021-05-18 10:31:34 -0300

    [feat] adiciona configs de seguranca

---
+++ src/main/java/zupacademy/magno/transacoes/config/seguranca/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package zupacademy.magno.transacoes.config.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "api/v1/transacoes/**").hasAuthority("SCOPE_JWT")
+                        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
